b549cb41e02e28320aedbd0cdd740dea5dcf93e4
sofia <250411@utxicotepec.edu.mx>
2026-09-17 14:00:03 -0600

    arreglos python

---
+++ list.py
@@ -0,0 +1,10 @@
+#RECORRIDO DE LISTA 1
+lista =[10, 20, 30, 40, 50, 60]
+#for x in lista:
+    #print(x)
+    #RECORRIDO DE LISTA 2
+    #for i in range(len(lista)):
+    #print(lista[i])
+    #print(lista[-1])
+#print(lista[::-1])
+print(lista[:3])
