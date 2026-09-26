dcb6e0327bd574b7e51c23aacbd8a69f189a381a
sofia <250411@utxicotepec.edu.mx>
2026-09-26 10:28:50 -0600

    funcion_con_parametros

---
+++ funcion_con_parametros.py
@@ -0,0 +1,23 @@
+# Función 1: Saludar
+def saludar(nombre):
+    print("Hola", nombre)
+
+
+# Función 2: Sumar dos números
+def sumar(a, b):
+    resultado = a + b
+    print("La suma es:", resultado)
+
+
+# Función 3: Calcular el área de un rectángulo
+def area_rectangulo(base, altura):
+    area = base * altura
+    print("El área del rectángulo es:", area)
+
+
+# Llamar a las funciones
+saludar("Frida")
+
+sumar(10, 5)
+
+area_rectangulo(8, 4)
